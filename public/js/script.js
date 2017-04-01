ffc4f45b5070fa076d66e5a07be22009a877ce02
karan9 <karan.srivastava@protonmail.com>
2017-04-01 18:07:42 +0530

    Initial Website Push

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+(function($) {
+  $("#text-heading").typeIt({
+     strings: ["I'm a Developer", "I'm a Blogger", "Karan Srivastava"],
+     speed: 130,
+     startDelay: 110,
+     breakLines: false,
+     autoStart: false
+  })
+})(jQuery);
